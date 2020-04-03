103a1a47b6895dccb388492accd27ce2155c0000
Ваше Имя <you@example.com>
2020-04-03 19:04:37 +0300

    1

--- kursovik/reshenie.cpp
+++ kursovik/reshenie.cpp
@@ -3,9 +3,16 @@
 //        5.2. Полный перебор множества допустимый решений.
 
 #include <iostream> 
+#include <vector>
 #include "main.h"
 
 using  namespace  std;
+
+struct node
+{
+	int i;
+	int j;
+};
 
 void reshenie()
 {
@@ -22,7 +29,82 @@ void reshenie()
 
 void reshenie_1()
 {
-	//удалим лишние переходы
+	vector<int> result;	
+
+	//search min
+	int imin=0, jmin=0, ib, jb;
+	int *begin, *end;
+	double tmin = T[0][1];
+	double tmini,tminj;
+	for (int i=0; i<N; i++)
+		for (int j=0; j<N; j++)
+			if(i != j && T[i][j] < tmin)
+			{
+				tmin = T[i][j];
+				imin = i;
+				jmin = j;
+			}
+	result.push_back(imin);
+	result.push_back(jmin);
+
+	while( result.size() != N )
+	{
+		begin = result.begin();
+		end = result.end();
+		//find i not in result
+		for (int i=0; i<N; i++)
+			if(find(begin,end,i) == end)
+			{
+				imin = i;
+				break;
+			}
+
+		tmin = T[imin][*begin];
+
+		for (int i=imin; i<N; i++)
+			if(i != j && find(begin,end,i) == end; && T[i][*begin] < tmini)
+			{
+				tmini = T[i][*begin];
+				imin = i;
+			}
+
+		result.insert(begin, imin);
+/*
+		tminj = j;
+		for (int j=0; j<N; j++)
+			if(i != j && T[result.end()][j] < tmin)
+			{
+				tmin = T[result.end()][j];
+				jmin = j;
+			}
+
+*/
+	
+		for(auto x: result )
+			cout << x+1 << " ";
+		cout << endl;
+	
+	}
+	
+/*
+	//sort array by time
+	double tmin = T[0][0];
+	double* Tsort = new double[N*N];
+	for (int k=0; k<N; k++)
+		for (int l=0; l<N; l++)
+		{
+			for (int i=k; i<N; i++)
+				for (int j=l; j<N; j++)
+					if(i != j)
+						if(T[i][j] < tmin)
+						{
+							tmin = T[i][j];
+							imin = i;
+							jmin = j;
+						}
+*/
+
+
 	
 }
 
