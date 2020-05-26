e5aa8f98e1b40b685c68d1deb488e9b56ba7d286
Ваше Имя <you@example.com>
2020-05-26 06:02:19 +0300

    1

--- labs/7.cpp
+++ labs/7.cpp
@@ -20,13 +20,8 @@ void print(struct node* leaf)
 		print(leaf->right);
 }
 
-
-int main()
+void create_src_trees(struct node* tree1, struct node* tree2, int a[], int b[] )
 {
-	int a[] = {1,2,3,4,5,6,7};
-	int b[] = {2,4,6,8,10,12,14};
-
-	struct node* tree1 = new struct node;
 	tree1->data = a[0];
 	tree1->left = new struct node;
 	tree1->right = new struct node;
@@ -49,7 +44,6 @@ int main()
 	tree1->right->right->left = NULL;
 	tree1->right->right->right = NULL;
 
-	struct node* tree2 = new struct node;
 	tree2->data = b[0];
 	tree2->left = new struct node;
 	tree2->right = new struct node;
@@ -71,11 +65,53 @@ int main()
 	tree2->right->left->right = NULL;
 	tree2->right->right->left = NULL;
 	tree2->right->right->right = NULL;
+}
+
+void add_new_data(int data, struct node* tree )
+{
+	if(!tree)
+	{
+		tree = new struct node;
+		tree->left = NULL;
+		tree->right = NULL;
+		tree->data = data;
+	}
+	else
+		if(data < tree->data)
+			add_new_data(data, tree->left );
+		else
+			add_new_data(data, tree->right );
+}
+
+void add_new_tree(struct node* src_tree, struct node* dst_tree )
+{
+	add_new_data(src_tree->data, dst_tree );
+
+	if(src_tree->left)
+		add_new_data(src_tree->left->data, dst_tree);
+	if(src_tree->right)
+		add_new_data(src_tree->right->data, dst_tree);
+}
+
+int main()
+{
+	int a[] = {1,2,3,4,5,6,7};
+	int b[] = {2,4,6,8,10,12,14};
+
+	struct node* tree1 = new struct node;
+	struct node* tree2 = new struct node;
+	struct node* tree3 ;
+
+	create_src_trees(tree1, tree2, a, b);
+	add_new_tree(tree1, tree3);
+	add_new_tree(tree2, tree3);
 
 	cout << "Tree 1" << endl;
 	print(tree1);
 	cout << endl;
 	cout << "Tree 2" << endl;
 	print(tree2);
+	cout << "Tree 3" << endl;
+	print(tree3);
 	return 0;
 }
