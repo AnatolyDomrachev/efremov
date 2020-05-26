07bc682504d251fe0c8672480118fcc06dfd7c50
Ваше Имя <you@example.com>
2020-05-26 09:55:56 +0300

    1

--- labs/7.cpp
+++ labs/7.cpp
@@ -20,51 +20,29 @@ void print(struct node* leaf)
 		print(leaf->right);
 }
 
-void create_src_trees(struct node* tree1, struct node* tree2, int a[], int b[] )
+void create_src_tree(struct node* tree, int a[])
 {
-	tree1->data = a[0];
-	tree1->left = new struct node;
-	tree1->right = new struct node;
-	tree1->left->data = a[1];
-	tree1->right->data = a[2];
-	tree1->left->left = new struct node;
-	tree1->left->right = new struct node;
-	tree1->right ->left = new struct node;
-	tree1->right ->right = new struct node;
-	tree1->left->left->data = a[3];
-	tree1->left->right->data = a[4];
-	tree1->right->left->data = a[5];
-	tree1->right->right->data = a[6];
-	tree1->left->left->left = NULL;
-	tree1->left->left->right = NULL;
-	tree1->left->right->left = NULL;
-	tree1->left->right->right = NULL;
-	tree1->right->left->left = NULL;
-	tree1->right->left->right = NULL;
-	tree1->right->right->left = NULL;
-	tree1->right->right->right = NULL;
-
-	tree2->data = b[0];
-	tree2->left = new struct node;
-	tree2->right = new struct node;
-	tree2->left->data = b[1];
-	tree2->right->data = b[2];
-	tree2->left->left = new struct node;
-	tree2->left->right = new struct node;
-	tree2->right ->left = new struct node;
-	tree2->right ->right = new struct node;
-	tree2->left->left->data = b[3];
-	tree2->left->right->data = b[4];
-	tree2->right->left->data = b[5];
-	tree2->right->right->data = b[6];
-	tree2->left->left->left = NULL;
-	tree2->left->left->right = NULL;
-	tree2->left->right->left = NULL;
-	tree2->left->right->right = NULL;
-	tree2->right->left->left = NULL;
-	tree2->right->left->right = NULL;
-	tree2->right->right->left = NULL;
-	tree2->right->right->right = NULL;
+	tree->data = a[0];
+	tree->left = new struct node;
+	tree->right = new struct node;
+	tree->left->data = a[1];
+	tree->right->data = a[2];
+	tree->left->left = new struct node;
+	tree->left->right = new struct node;
+	tree->right ->left = new struct node;
+	tree->right ->right = new struct node;
+	tree->left->left->data = a[3];
+	tree->left->right->data = a[4];
+	tree->right->left->data = a[5];
+	tree->right->right->data = a[6];
+	tree->left->left->left = NULL;
+	tree->left->left->right = NULL;
+	tree->left->right->left = NULL;
+	tree->left->right->right = NULL;
+	tree->right->left->left = NULL;
+	tree->right->left->right = NULL;
+	tree->right->right->left = NULL;
+	tree->right->right->right = NULL;
 }
 
 void add_new_data(int data, struct node** tree )
@@ -88,9 +66,9 @@ void add_new_tree(struct node* src_tree, struct node** dst_tree )
 	add_new_data(src_tree->data, dst_tree );
 
 	if(src_tree->left)
-		add_new_data(src_tree->left->data, dst_tree);
+		add_new_tree(src_tree->left, dst_tree);
 	if(src_tree->right)
-		add_new_data(src_tree->right->data, dst_tree);
+		add_new_tree(src_tree->right, dst_tree);
 }
 
 int main()
@@ -102,7 +80,8 @@ int main()
 	struct node* tree2 = new struct node;
 	struct node* tree3 = NULL;
 
-	create_src_trees(tree1, tree2, a, b);
+	create_src_tree(tree1, a);
+	create_src_tree(tree2, b);
 
 	add_new_tree(tree1, &tree3);
 	add_new_tree(tree2, &tree3);
